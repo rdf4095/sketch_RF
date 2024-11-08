84f42652611a4ae503703d15a9fa4d514434f1b1
rdf4095 <112559769+rdf4095@users.noreply.github.com>
2024-11-08 17:05:57 -0500

    debug draw_line, add canvas to hold colorbar

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # sketch_RF
-Interactive drawing application
+An interactive drawing application (app)
+
+The app defines a tkinter canvas of arbitrary size, which responds to various
+user actions, including:
+- left mouse button, successive clicks define lines.
+- left mouse + drag, defines freehand paths, pixel by pixel.
+- left mouse double-click, defines a line that joins the current point
+  to the first point that was single-clicked.
+
--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 """
 program: main.py
 
-purpose: For projecct sketch_RF.
-         Interactive drawing.
+purpose: For project sketch_RF, interactive drawing.
 
 comments: Supports freehand drawing by following cursor movement,
           or polygonal lines by drawing from mouse-click to mouse-click.
@@ -14,6 +13,8 @@ history:
 10-31-2024  creation
 11-04-2024  In draw_line, allow "closing" to the start point.
             Add mouse movement threshold for freehand drawing
+11-08-2024  Hide 2nd canvas. Debug draw_line(). Remove old tests and comments.
+            Display colorbar for selecting line color (no functionality yet.)
 """
 
 import tkinter as tk
@@ -30,12 +31,12 @@ class Sketchpad(tk.Canvas):
 
         super().__init__(parent, **kwargs)
         self.bind("<Button-1>", self.draw_line)
-        self.bind("<Double-1>", self.end_line)
+        self.bind("<Double-1>", self.connect_line)
         self.bind("<B1-Motion>", self.draw_path)
         
-    def save_posn(self, event):
+    def save_posn(self, event) -> None:
         """
-        the mouse cursor position (posn) attributes depend on context:
+        the mouse cursor position (posn) depends on context:
         .winfo_pointerx,y is the posn of the cursor relative to the screen.
         .winfo_rootx,y is the posn of the root object relative to the screen.
         event.x,y is the posn of cursor relative to the root window.
@@ -53,42 +54,44 @@ class Sketchpad(tk.Canvas):
         self.lastx, self.lasty = event.x, event.y
 
 
-    def save_start_posn(self, event):
-        # pass
+    def save_start_posn(self, event) -> None:
+        """Save the x-y coordinates of the first left-mouse click."""
         self.startx, self.starty = event.x, event.y
         print(f'starting: {self.startx},{self.starty}')
 
 
     def draw_line(self, event) -> None:
+        """If past the starting position, draw a line from last posn to current posn."""
         if (self.startx == 0 and self.starty == 0):
-            # print('    no startx, saving it')
             self.save_start_posn(event)
+            self.save_posn(event)
             return
         
-        if (self.lastx == 0 and self.lasty == 0):
-            # print('    no lastx, saving it')
-            self.save_posn(event)
-            self.create_line(self.startx, self.starty, event.x, event.y, fill='red', width=self.linewidth)
-            return
-
-        # print('    drawing line')
         self.create_line(self.lastx, self.lasty, event.x, event.y, fill='red', width=self.linewidth)
         self.save_posn(event)
 
 
-    def end_line(self, event):
+    def connect_line(self, event) -> None:
+        """Draw a line from the current position to the starting position."""
         # print('in end_line')
         # print(f'    drawing from {event.x},{event.y} to {self.startx},{self.starty}')
         self.create_line(event.x, event.y, self.startx, self.starty, fill='blue', width=self.linewidth)
 
 
-    def draw_path(self, event):
-        threshold = 4
-        # print('    drawing path:')
-        # print(f'    {self.lastx},{self.lasty} to {event.x},{event.y}')
-        if (abs(event.x - self.lastx) > threshold and abs(event.y - self.lasty) > threshold):
+    def draw_path(self, event) -> None:
+        x_threshold = 1
+        y_threshold = 1
+
+        # draw with threshold for the amount of cursor movement
+        if (abs(event.x - self.lastx) >= x_threshold and abs(event.y - self.lasty) >= y_threshold):
             self.create_line(self.lastx, self.lasty, event.x, event.y, fill='red', width=self.linewidth)
             self.save_posn(event)
+
+        # always draw
+        # if (abs(event.x - self.lastx) < threshold and abs(event.y - self.lasty) < threshold):
+        #     self.create_line(self.lastx, self.lasty, event.x, event.y, fill='red', width=self.linewidth)
+        #     self.save_posn(event)
+
 
 
 root = tk.Tk()
@@ -101,22 +104,28 @@ root.rowconfigure(0, weight=1)
 root.columnconfigure(1, weight=1)
 root.rowconfigure(1, weight=1)
 
-sketch = Sketchpad(root, width=300, height=300, background='#ff0')
-# 'sticky' expands the canvas to size of parent
-# sketch.grid(column=0, row=0, pady=5, sticky=(tk.N, tk.W, tk.E, tk.S))
-sketch.grid(column=0, row=0, pady=5)
+sketch = Sketchpad(root, width=320, height=320, background='#ff0')
+sketch.grid(column=0, row=0)
 
 # canv2 = tk.Canvas(root, width=300, height=200, background='#fa0')
-# there is a default size
-canv2 = tk.Canvas(root, background='#fa0')
-
-# 'sticky' expands the canvas to size of parent
-canv2.grid(column=0, row=1, pady=5, sticky=('n', 's', 'e', 'w'))
+# there is a default canvas size
+# canv2 = tk.Canvas(root, )#, background='#fa0')
 # canv2.grid(column=0, row=1, pady=5)
 
-# canv2.grid(column=0, row=1, padx=5, pady=5)
+colorbar = tk.Canvas(root, height=40)
+colorbar.grid(column=0, row=1, sticky='ew')
+colorbar.update()
+# works:
+# print(f'size of colorbar: {colorbar.winfo_width()}')
 
-# canv2.update()
+# fill canvas with 8 color samples
+num_colors = 8      # not used yet
+colors = ['red', 'orange', 'yellow', 'green', 'blue', 'violet', 'magenta', 'black']
+xs = list(range(0, 320, 40))
+y1 = 0
+y2 = 42
+for n, x in enumerate(xs):
+    colorbar.create_rectangle(x, y1, x+40, y2, fill=colors[n])
 
 btnq = ttk.Button(root,
                   text="Quit",
